60862ee97ca6d1d8d6d8c022b1830f73b75b6396
Ali Momeni <batchku@gmail.com>
2017-05-24 11:04:37 -0400

    initial commit

---
+++ addons.make
@@ -0,0 +1,3 @@
+ofxOsc
+ofxSyphon
+ofxXmlSettings
---
+++ src/ofApp.cpp
@@ -0,0 +1,116 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    
+
+    XML.loadFile("mySettings.xml");
+    camWidth = XML.getValue("camWidth", 0);  // try to grab at this size.
+    camHeight = XML.getValue("camHeight", 0);
+
+    //we can now get back a list of devices.
+    vector<ofVideoDevice> devices = vidGrabber.listDevices();
+
+    for(int i = 0; i < devices.size(); i++){
+        if(devices[i].bAvailable){
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName;
+        }else{
+            ofLogNotice() << devices[i].id << ": " << devices[i].deviceName << " - unavailable ";
+        }
+    }
+
+    
+ 
+    
+    vidGrabber.setDeviceID(XML.getValue("cameraDeviceID", 0));
+    vidGrabber.setDesiredFrameRate(60);
+    vidGrabber.initGrabber(camWidth, camHeight);
+
+    ofSetVerticalSync(true);
+    
+    //setup syphon
+    
+    individualTextureSyphonServer.setName("webcam2syphon");
+    
+    
+   }
+
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    ofBackground(100, 100, 100);
+    vidGrabber.update();
+
+    if(vidGrabber.isFrameNew()){
+        //ofPixels & pixels = vidGrabber.getPixels();
+        videoTexture = vidGrabber.getTexture();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetHexColor(0xffffff);
+    //vidGrabber.draw(20, 20);
+    individualTextureSyphonServer.publishTexture(&videoTexture);
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    // in fullscreen mode, on a pc at least, the 
+    // first time video settings the come up
+    // they come up *under* the fullscreen window
+    // use alt-tab to navigate to the settings
+    // window. we are working on a fix for this...
+
+    // Video settings no longer works in 10.7
+    // You'll need to compile with the 10.6 SDK for this
+    // For Xcode 4.4 and greater, see this forum post on instructions on installing the SDK
+    // http://forum.openframeworks.cc/index.php?topic=10343
+    if(key == 's' || key == 'S'){
+        vidGrabber.videoSettings();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+}
